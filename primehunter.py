ee9cb20bb5482d728d417e7dd21fbab7129b0937
onereborn <d2mharry147@hotmail.com>
2026-06-10 12:23:01 +0100

    added file saving and found first 500 digit prime

--- primehunter.py
+++ primehunter.py
@@ -1,5 +1,6 @@
+import os
 import random
-import math
+import datetime
 
 def generate_candidate(min_digits):
 
@@ -16,9 +17,57 @@ def count_digits(number):
     return len(str(number))
 
 def main():
-    candidate = generate_candidate(100)
-    print(f"Generated candidate: {candidate}")
-    print(f"Number of digits: {count_digits(candidate)}")
+    attempts = 0
+    
+    while True:
+        attempts += 1
+        candidate = generate_candidate(500)
+        if is_prime(candidate):
+            print(f"Found a prime number: {candidate} after {attempts} attempts")
+            break
+
+        if attempts % 100 == 0:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print(f"Attempt {attempts}: Still searching for a prime number...")   
+
+        save_prime_to_file(candidate, attempts)
+
+def is_prime(n, rounds = 10):
+
+    if n == 2 or n == 3:
+        return True
+    
+    if n % 2 == 0 or n < 2:
+        return False
+    
+    d = n - 1
+    r = 0
+    while d % 2 == 0:
+        d = d // 2
+        r += 1
+
+    for _ in range(rounds):
+        a = random.randint(2, n - 1)
+        x = pow(a, d, n)
+
+        if x == 1 or x == n - 1:
+            continue
+        
+        for _ in range(r - 1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                break
+        else:
+            return False
+    return True
+
+def save_prime_to_file(prime, attempts, filename="found_primes.txt"):
+    with open(filename, "a") as file:
+        file.write(f"Prime found: {prime}" + "\n")
+        file.write(f"=================================" + "\n")
+        file.write(f"Digits: {count_digits(prime)}" + "\n")
+        file.write(f"Timestamp: {datetime.datetime.now()}" + "\n")
+        file.write(f"attempts: {attempts}" + "\n")
 
 if __name__ == "__main__":
     main()
